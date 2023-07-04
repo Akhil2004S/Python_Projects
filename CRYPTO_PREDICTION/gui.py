b4caae72aef6920f52a1a64546b08e0a34121730
DarkLawliet <126699404+DarkLawliet@users.noreply.github.com>
2023-07-04 09:49:41 +0530

    Delete gui.py

--- CRYPTO_PREDICTION/gui.py
+++
@@ -1,5 +0,0 @@
-import gradio as gr
-x = gr.Interface(fn=crypto_prediction,
-                 inputs=['text','number','number','number'],
-                 outputs=['number'])
-x.launch(debug = True)
